12a472b7664aaebd5b9199b7732d71a13158ddab
fanqilong <fanql@jxgtmbjt.com>
2019-04-03 11:44:12 +0800

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-vue-tv'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
